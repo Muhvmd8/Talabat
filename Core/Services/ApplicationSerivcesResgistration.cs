fa5178d2ab7eeaee390f4739cce95b1159da1ac3
Muhvmd8 <ma7007167@gmail.com>
2025-08-21 00:07:28 +0300

    Refactor Service Manager

--- Core/Services/ApplicationSerivcesResgistration.cs
+++ Core/Services/ApplicationSerivcesResgistration.cs
@@ -7,7 +7,31 @@ public static class ApplicationSerivcesResgistration
         {
             config.AddMaps(typeof(AssemblyReference).Assembly);
         }, typeof(AssemblyReference).Assembly);
-        services.AddScoped<IServiceManager, ServiceManager>();
+
+        services.AddScoped<IServiceManager, ServiceManagerWithFactoryDelegate>();
+
+        #region Register Application Services With Its Dependencies Using Factory Delegate
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<Func<IProductService>>(provider =>
+            () => provider.GetRequiredService<IProductService>()
+        );
+
+        services.AddScoped<IBasketService, BasketService>();
+        services.AddScoped<Func<IBasketService>>(provider =>
+            () => provider.GetRequiredService<IBasketService>()
+        );
+
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<Func<IAuthenticationService>>(provider =>
+            () => provider.GetRequiredService<IAuthenticationService>()
+        );
+
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<Func<IOrderService>>(provider =>
+            () => provider.GetRequiredService<IOrderService>()
+        );
+        #endregion
+
         return services;
     }
 }
---
+++ Core/Services/ServiceManagerWithFactoryDelegate.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Services;
+public class ServiceManagerWithFactoryDelegate(Func<IProductService> _productFactory,
+    Func<IBasketService> _basketFactory,
+    Func<IAuthenticationService> _authenticationFactory,
+    Func<IOrderService> _orderFactory)
+    : IServiceManager
+{
+    public IProductService ProductService => _productFactory();
+    public IBasketService BasketService => _basketFactory();
+    public IAuthenticationService AuthenticationService => _authenticationFactory();
+    public IOrderService OrderService => _orderFactory();
+}
